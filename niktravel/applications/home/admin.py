29613fd16d07d9bbaf93edc5cc7aa458f535eb0b
ikkerxd <henrry.saa@gmail.com>
2018-06-15 23:17:17 -0500

    enlazando vista de idne con el template

--- niktravel/applications/home/admin.py
+++ niktravel/applications/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.AdminSite.site_header = 'Adminstrador Inkatravel vip'
+admin.AdminSite.site_title = 'Inkatrvel Vip'
--- niktravel/applications/home/views.py
+++ niktravel/applications/home/views.py
@@ -1,5 +1,19 @@
+# django
 from django.views.generic import TemplateView
+
+# local
+from applications.tours.models import Tour
 
 
 class IndexView(TemplateView):
+    """
+    Vista de inicio(index) de inkatravel vip
+    """
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['ofertas'] = Tour.objects.all()
+        context['tours'] = Tour.objects.all()
+        return context
+    
